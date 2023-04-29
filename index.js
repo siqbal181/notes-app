9d47bd6b78b2509a172eff7c971178424ba6db0c
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-04-29 18:07:55 +0100

    Added model to index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const notesModel = require('./notesModel');
+
 console.log('The note app is running')
+
+const notes = new notesModel;
+notes.addNote('Buy milk')
+notes.addNote('Go to gym')
+console.log(notes.getNotes());
